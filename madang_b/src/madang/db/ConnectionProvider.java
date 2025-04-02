6a45f592a08a2266270a838285fdbbce5c26d4d3
tinytinalee <leewon0209@naver.com>
2025-04-02 11:53:35 +0900

    feat: connection provider

--- madang_b/src/madang/db/ConnectionProvider.java
+++ madang_b/src/madang/db/ConnectionProvider.java
@@ -1,5 +1,56 @@
 package madang.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ConnectionProvider {
+	public static String driver = "oracle.jdbc.driver.OracleDriver";
+	public static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	public static String userid = "c##madang";
+	public static String pwd = "madang";
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, userid, pwd);
+		} catch (Exception e) {
+			System.out.println("예외발생: "+e.getMessage());
+		}
+		
+		return conn;
+	}
+	
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if(rs != null) {
+				rs.close();
+			}
+			if(stmt != null) {
+				stmt.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			System.out.println("예외발생: "+e.getMessage());
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			if(stmt != null) {
+				stmt.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			System.out.println("예외발생: "+e.getMessage());
+		}
+	}
 	
 }
